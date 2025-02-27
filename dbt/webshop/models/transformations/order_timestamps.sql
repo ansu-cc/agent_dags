e1d282fb1b9e3bea51a2631d9a932b19926fe181
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 12:58:16 +0530

    Update order_timestamps.sql

--- dbt/webshop/models/transformations/order_timestamps.sql
+++ dbt/webshop/models/transformations/order_timestamps.sql
@@ -1,29 +1,3 @@
--- {{ config(
---     materialized='table',
---     alias='updated_order'
--- ) }}
-
--- WITH date_shift AS (
---     SELECT 
---         MAX(orderTimestamp) AS max_timestamp,
---         DATE_PART('day', CURRENT_DATE - MAX(orderTimestamp)) AS shift_days
---     FROM {{ source('webshop_v2', 'updated_order') }}
--- ),
--- shifted_orders AS (
---     SELECT 
---         id,
---         customerid,
---         orderTimestamp + (INTERVAL '1 day' * date_shift.shift_days) AS orderTimestamp,
---         shippingaddressid,
---         total,
---         shippingcost,
---         created,
---         updated
---     FROM {{ source('webshop_v2', 'updated_order') }}, date_shift
--- )
-
--- SELECT * FROM shifted_orders
-
 {{ config(
     materialized='incremental',
     unique_key='id'
