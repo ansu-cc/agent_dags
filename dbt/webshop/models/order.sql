086819f32d1eb45a79d154d521bbf31a3816aee0
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 14:25:02 +0530

    Update order.sql

--- dbt/webshop/models/order.sql
+++ dbt/webshop/models/order.sql
@@ -6,13 +6,13 @@
 SELECT
     id,
     customerid,
-    ordertimestamp + (NOW() - MAX(ordertimestamp) OVER())::INTERVAL AS ordertimestamp,
+    ordertimestamp + (CURRENT_DATE - MAX(ordertimestamp) OVER()) AS ordertimestamp,
     shippingaddressid,
     total,
     shippingcost,
     created,
     updated
-FROM webshop_v2.order
+FROM webshop_v2.order_seed
 
 {% if is_incremental() %}
 WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
