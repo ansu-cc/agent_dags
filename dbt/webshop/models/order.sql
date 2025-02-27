2156f98947951523e5cbc0f61f38ea6f6d29d80b
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 14:50:06 +0530

    Update order.sql

--- dbt/webshop/models/order.sql
+++ dbt/webshop/models/order.sql
@@ -1,28 +1,22 @@
 {{ config(
-    materialized='incremental',
-    unique_key='id'
+    materialized='table'
 ) }}
 
-WITH date_shift AS (
-    SELECT
-        MAX(ordertimestamp)::TIMESTAMP AS max_timestamp,  -- Ensure correct type
-        DATE_PART('day', CURRENT_DATE - MAX(ordertimestamp)::DATE) AS shift_days
-    FROM {{ ref('order_seed') }}  -- Use ref() for dbt-managed tables
+WITH max_time AS (
+    SELECT MAX(ordertimestamp) AS max_timestamp FROM {{ ref('order_seed') }}
 ),
-shifted_orders AS (
-    SELECT
-        id,
-        customerid,
-        ordertimestamp + (INTERVAL '1 day' * date_shift.shift_days) AS ordertimestamp,
-        shippingaddressid,
-        total,
-        shippingcost,
-        created,
-        updated
-    FROM {{ ref('order_seed') }}, date_shift
+
+day_diff AS (
+    SELECT DATE_PART('day', NOW() - max_timestamp) AS day_difference FROM max_time
 )
 
-SELECT * FROM shifted_orders
-{% if is_incremental() %}
-WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
-{% endif %}
+SELECT 
+    id,
+    customerid,
+    ordertimestamp + INTERVAL '1 day' * (SELECT day_difference FROM day_diff) AS updated_ordertimestamp,
+    shippingaddressid,
+    total,
+    shippingcost,
+    created,
+    updated
+FROM {{ ref('order_seed') }}
