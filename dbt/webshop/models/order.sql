205ef7dcfba590ea0d921a6023dc169b7b3f5bfa
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 14:38:53 +0530

    Update order.sql

--- dbt/webshop/models/order.sql
+++ dbt/webshop/models/order.sql
@@ -4,13 +4,13 @@
 ) }}
 
 WITH date_shift AS (
-    SELECT 
+    SELECT
         MAX(ordertimestamp)::TIMESTAMP AS max_timestamp,  -- Ensure correct type
         DATE_PART('day', CURRENT_DATE - MAX(ordertimestamp)::DATE) AS shift_days
     FROM {{ ref('order_seed') }}  -- Use ref() for dbt-managed tables
 ),
 shifted_orders AS (
-    SELECT 
+    SELECT
         id,
         customerid,
         ordertimestamp + (INTERVAL '1 day' * date_shift.shift_days) AS ordertimestamp,
@@ -20,9 +20,11 @@ shifted_orders AS (
         created,
         updated
     FROM {{ ref('order_seed') }}, date_shift
-    {% if is_incremental() %}
-    WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
-    {% endif %}
 )
 
-SELECT * FROM shifted_orders;
+-- Ensure proper WHERE condition for incremental models
+SELECT * FROM shifted_orders
+{% if is_incremental() %}
+WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
+{% endif %};
+
