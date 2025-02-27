63c03caeec3bda5bc4165ff34daefb2150968ba1
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 12:45:35 +0530

    Update order_timestamps.sql

--- dbt/webshop/models/transformations/order_timestamps.sql
+++ dbt/webshop/models/transformations/order_timestamps.sql
@@ -1,25 +1,45 @@
+-- {{ config(
+--     materialized='table',
+--     alias='updated_order'
+-- ) }}
+
+-- WITH date_shift AS (
+--     SELECT 
+--         MAX(orderTimestamp) AS max_timestamp,
+--         DATE_PART('day', CURRENT_DATE - MAX(orderTimestamp)) AS shift_days
+--     FROM {{ source('webshop_v2', 'updated_order') }}
+-- ),
+-- shifted_orders AS (
+--     SELECT 
+--         id,
+--         customerid,
+--         orderTimestamp + (INTERVAL '1 day' * date_shift.shift_days) AS orderTimestamp,
+--         shippingaddressid,
+--         total,
+--         shippingcost,
+--         created,
+--         updated
+--     FROM {{ source('webshop_v2', 'updated_order') }}, date_shift
+-- )
+
+-- SELECT * FROM shifted_orders
+
 {{ config(
-    materialized='table',
-    alias='updated_order'
+    materialized='incremental',
+    unique_key='id'
 ) }}
 
-WITH date_shift AS (
-    SELECT 
-        MAX(orderTimestamp) AS max_timestamp,
-        DATE_PART('day', CURRENT_DATE - MAX(orderTimestamp)) AS shift_days
-    FROM {{ source('webshop_v2', 'updated_order') }}
-),
-shifted_orders AS (
-    SELECT 
-        id,
-        customerid,
-        orderTimestamp + (INTERVAL '1 day' * date_shift.shift_days) AS orderTimestamp,
-        shippingaddressid,
-        total,
-        shippingcost,
-        created,
-        updated
-    FROM {{ source('webshop_v2', 'updated_order') }}, date_shift
-)
+SELECT
+    id,
+    customerid,
+    ordertimestamp + (CURRENT_DATE - MAX(ordertimestamp) OVER()) AS ordertimestamp,
+    shippingaddressid,
+    total,
+    shippingcost,
+    created,
+    updated
+FROM webshop_v2.updated_order
 
-SELECT * FROM shifted_orders
+{% if is_incremental() %}
+WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
+{% endif %}
