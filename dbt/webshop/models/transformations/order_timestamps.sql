c00743e34099b9a2d52d968975b909b0c63560b9
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-27 10:15:57 +0530

    Update order_timestamps.sql

--- dbt/webshop/models/transformations/order_timestamps.sql
+++ dbt/webshop/models/transformations/order_timestamps.sql
@@ -1,1 +1,23 @@
+WITH date_shift AS (
+    -- Find the max timestamp and calculate the number of days to shift
+    SELECT 
+        MAX(orderTimestamp) AS max_timestamp,
+        DATE_PART('day', CURRENT_DATE - MAX(orderTimestamp)) AS shift_days
+    FROM {{ source('webshop_webshop_v2', 'order') }}
+),
+updated_orders AS (
+    -- Apply the shift to all order timestamps
+    SELECT 
+        id,
+        customerid,
+        orderTimestamp + (INTERVAL '1 day' * (SELECT shift_days FROM date_shift)) AS orderTimestamp,
+        shippingaddressid,
+        total,
+        shippingcost,
+        created,
+        updated
+    FROM {{ source('webshop_webshop_v2', 'order') }}
+)
+
+SELECT * FROM updated_orders;
 
